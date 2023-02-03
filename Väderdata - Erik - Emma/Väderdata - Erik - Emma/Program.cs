07efe81ce0f8e5b4249eacd548249201035cc4d3
Erik-Ga <115992708+Erik-Ga@users.noreply.github.com>
2023-02-03 09:48:51 +0100

    Init App 1.0

---
+++ Väderdata - Erik - Emma/Väderdata - Erik - Emma/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Väderdata___Erik___Emma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
